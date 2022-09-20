39d72fca9b212be5c77d8f1d5c2410e8e9ce7fe0
master <wjddn3711@github.com>
2022-09-21 01:29:14 +0900

    1. JsonIgnore 를 통하여 json 반환 객체의 커스터마이징 2. static filtering 으로 모든 api 에서 호출된 빈에서 List 이건 필드이건 모두 제외시킨다 3. 클래스에서는 JsonIgnoreProperties, 필드에서는 JsonIgnore 하지만 직관적인 코딩을 위해, JsonIgnore 를 선호한다

---
+++ src/main/java/com/in28menutes/rest/webservices/restfulweb0services/filtering/SomeBean.java
@@ -0,0 +1,35 @@
+package com.in28menutes.rest.webservices.restfulweb0services.filtering;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties("field1")
+public class SomeBean {
+    private String field1;
+    @JsonIgnore
+    private String field2;
+    private String field3;
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+
+}
