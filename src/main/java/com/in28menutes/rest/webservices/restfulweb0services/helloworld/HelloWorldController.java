8c8b29001496a7479049ad6b0e1badc5a877f807
master <wjddn3711@github.com>
2022-09-21 01:29:38 +0900

    국제화 실습

--- src/main/java/com/in28menutes/rest/webservices/restfulweb0services/helloworld/HelloWorldController.java
+++ src/main/java/com/in28menutes/rest/webservices/restfulweb0services/helloworld/HelloWorldController.java
@@ -1,10 +1,19 @@
 package com.in28menutes.rest.webservices.restfulweb0services.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 // Rest API
 @RestController // marking this contorller as rest controller
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
 
     // hello-world
     // Mapping Request as Get Method, and map path
@@ -29,5 +38,18 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(
                 String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale(); // 현재 스레드와 연관된 로케일, 만약 없다면 default Locale
+        return messageSource.getMessage("good.morning.message", null, "default Message", locale);
+
+        /*
+        en - English = Good Morning
+        ko - Korean = 좋은 아침
+        fr - French = Bonjour
+        nl - Dutch = Goedemorgen
+         */
     }
 }
