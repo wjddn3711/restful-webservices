f41a9d3ac55453c8672c6be62582b4e0773c489a
master <wjddn3711@github.com>
2022-09-25 15:05:10 +0900

    JPA 설정을 추가하면서 Jpa Auditing 추가

--- src/main/java/com/in28menutes/rest/webservices/restfulweb0services/RestfulWeb0servicesApplication.java
+++ src/main/java/com/in28menutes/rest/webservices/restfulweb0services/RestfulWeb0servicesApplication.java
@@ -2,8 +2,10 @@ package com.in28menutes.rest.webservices.restfulweb0services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RestfulWeb0servicesApplication {
 
     public static void main(String[] args) {
